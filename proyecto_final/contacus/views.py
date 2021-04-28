607cc8224ff4bbea1d7e8d6743185aa07efa2384
Brayan <brayanjovegas@gmail.com>
2021-04-28 15:34:00 -0400

    agregado mensaje de envio exitoso en form de contacto

--- proyecto_final/contacus/views.py
+++ proyecto_final/contacus/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from contacus.forms import FormcContact
 
 
@@ -6,5 +6,15 @@ from contacus.forms import FormcContact
 def contacus(request):
 
     forms_contacus=FormcContact()
+
+    if request.method=="POST":
+        forms_contacus=FormcContact(data=request.POST)
+        if forms_contacus.is_valid():
+            nombre=request.POST.get("name")
+            apellido=request.POST.get("last_name")
+            email=request.POST.get("email")
+            mensaje=request.POST.get("mensaje")
+
+            return redirect("/contacus/?Correcto")
 
     return render(request, "proyecto_final/contacus.html",{"Formulario":forms_contacus})
