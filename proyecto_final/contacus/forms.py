adf6338030ef01abffd6fb8694648f0191740e0d
Brayan <brayanjovegas@gmail.com>
2021-04-28 14:09:17 -0400

    Creado vista de contactos

--- proyecto_final/contacus/forms.py
+++ proyecto_final/contacus/forms.py
@@ -5,5 +5,5 @@ class FormcContact(forms.Form):
     name=forms.CharField(label="Nombre", required=True)
     last_name=forms.CharField(label="Apellido", required=True)
     email=forms.EmailField(label="Email", required=True)
-    contenido=forms.CharField(label="Comentarios", required=True)
+    mensaje=forms.CharField(label="Mensaje", required=True, widget=forms.Textarea, max_length= 300)
     
