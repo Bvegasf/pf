d3f0eb77fecd5bc53478879c9ac68d0480047098
Brayan <brayanjovegas@gmail.com>
2021-04-29 16:21:46 -0400

    Agregado funcionalidad de envio de email a app servicio

--- proyecto_final/contacus/views.py
+++ proyecto_final/contacus/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import redirect, render
 from contacus.forms import FormcContact
+from django.core.mail import EmailMessage
+from django.conf import settings
 
 
 # Create your views here.
@@ -9,12 +11,25 @@ def contacus(request):
 
     if request.method=="POST":
         forms_contacus=FormcContact(data=request.POST)
+
         if forms_contacus.is_valid():
             nombre=request.POST.get("name")
             apellido=request.POST.get("last_name")
             email=request.POST.get("email")
             mensaje=request.POST.get("mensaje")
 
-            return redirect("/contacus/?Correcto")
+            correo=EmailMessage("Contacto App Django", "El usuario: {} {} con la direccion de correo electronico {}, escribe el siguiente mensaje: \n\n {} ".format(nombre,apellido,email,mensaje) , "" , ["brayanjovegas@gmail.com"], reply_to=[email])
+            try:
+                correo.send()
+                return redirect("/contacus/?Correcto")
+
+            except:
+                return redirect("/contacus/?Incorrecto")
+
+
+        
+
+            
+
 
     return render(request, "proyecto_final/contacus.html",{"Formulario":forms_contacus})
--- proyecto_final/proyecto_final/settings.py
+++ proyecto_final/proyecto_final/settings.py
@@ -129,3 +129,12 @@ STATICFILES_DIRS= (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL='/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# configuracion de envio de email
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER="brayanjovegas@gmail.com"
+EMAIL_HOST_PASSWORD="brayanvegas20"
