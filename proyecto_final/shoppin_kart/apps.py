a88baf066ab0f28716c254dba10bb9156ecdff21
Brayan <brayanjovegas@gmail.com>
2021-05-03 16:51:13 -0400

    Creada app shopping Kart, creado clases del shppin kart

---
+++ proyecto_final/shoppin_kart/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ShoppinKartConfig(AppConfig):
+    name = 'shoppin_kart'
---
+++ proyecto_final/shoppin_kart/shopping.py
@@ -0,0 +1,81 @@
+
+
+# clase de funcionalidad del carrito
+
+class kart:
+
+    # constructor que maneja sesiones del carrito de compra
+    # si el carrito no ha sio creado, se crea, si existe, se iguala al carro existente.
+
+
+    def __init__(self,request):
+        self.request=request
+        self.session=request.session
+        kart=self.get("carro")
+
+        if not kart:
+            kart=self.session["carro"]={}
+
+
+        else:
+            kart=kart
+
+    #funcion para agregar productos
+
+    def add_product(self,product):
+
+        #si el producto no existe en el carro ve agrega
+
+        if(str(product.id) not in self.kart.keys()):
+            self.kart[product.id]={
+                "product_id":product.id,
+                "product_nombre":product.name,
+                "product_precio":product.price,
+                "product_cantidad":1,
+                "product_image":product.image.url
+
+
+
+            }
+
+        #si el producto exite, se suma una cantidad.
+
+        else:
+            for key , value in self.kart.items():
+                value["product_cantidad"]=value["product_cantidad"] + 1
+                break
+
+        self.save_kart()
+
+    #guardar estado del carrito
+
+    def save_kart(self):
+        self.session["carro"]=self.kart
+        self.session.modified=True
+
+
+    #eliminar producto(toda la cantidad guardada)
+
+    def delete_product(self, product):
+        product.id=str(product.id)
+        if product.id in self.kart:
+            del self.kart[product.id]
+            self.save_kart()
+
+    #restar una unidad del producto
+
+    def rest_product(self,product):
+        for key, value in self.kart.items():
+            if key==str(product.id):
+                value["product_cantidad"]=value["product_caantidad"]-1
+                if value["product_cantidad"] < 1:
+                    self.delete_product(product)
+                break
+
+        self.save.kart()
+
+    def clean_kart(self,product):
+        self.session["carro"]={}
+        self.session.modified=True
+
+
